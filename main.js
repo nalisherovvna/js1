cf3db18f1e69dd2f252cd6e033db28179ccf2e6c
nalisherovvna <mushknasiba@gmail.com>
2024-03-27 11:23:02 +0500

    it was my homework

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function min(a,b) {
+    let c;
+    c = Math.min(a,b);
+    alert("kiritgan ikkta sonni kichigi = " + c);
+}
+
+let birinchi = Number(prompt("birinchi sonni kiriting..."));
+let ikkinchi = Number(prompt("ikkinchi sonni kiritng..."));
+min(birinchi,ikkinchi);
